7ad7a2126c85e8574b69f4cd0994d62306a82de8
MarcDirven <marc.dirven@student.hu.nl>
2019-04-25 12:52:47 +0200

    added more comments about how the code is going to look like most likely

--- code/src/hazard_detection.cpp
+++ code/src/hazard_detection.cpp
@@ -1,11 +1,22 @@
 #include <hazard_detection.hpp>
 
 namespace r2d2::hazard_detection {
-    hazard_detection::hazard_detection_c(base_comm_c& comm):
+    hazard_detection_c::hazard_detection_c(base_comm_c& comm):
         base_module_c(comm) {
             /*comm.listen_for_frames({
                 Doens't exist yet
-                r2d2::frame_type::gas_detection_s
+                r2d2::frame_type::GAS_DETECTION
             });*/
+    }
+
+    void hazard_detection_c::process() {
+        /*comm.request(frame_type::GAS_DETECTION);
+        while (comm.has_data()) {
+            const auto frame = comm.get_data();
+            if (frame.request) {
+                continue;
+            }
+            const auto gas_value = frame.as_frame_type<frame_type::GAS_DETECTION>...*/
+        }
     }
 }
