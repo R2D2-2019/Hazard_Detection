e2a8546ef16e4881f0b2739433a2187071f07ec8
MarcDirven <marc.dirven@student.hu.nl>
2019-05-13 12:29:02 +0200

    created haard detection class

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 #include <ostream>
 #include <hazard_detection.hpp>
-//#define CATCH_CONFIG_MAIN
-//#include <catch.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 
 int main() {
     return 0;
