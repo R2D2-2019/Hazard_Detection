ad1718525104abb8b39a5a48dd8d849c84110a72
Marc <marc.dirven@student.hu.nl>
2019-05-16 22:21:14 +0200

    Added two parameters, tresholds

--- code/headers/hazard_detection.hpp
+++ code/headers/hazard_detection.hpp
@@ -4,8 +4,29 @@
 
 namespace r2d2::hazard_detection {
     class hazard_detection_c : public base_module_c {
+        const int tempeature_treshold, ppm_treshold;
+
     public:
-        hazard_detection_c(base_comm_c &base_comm);
+        /**
+         * Hazard Detection Constructor.
+         * The tempeature_treshold is set to 49 by default.
+         * This is the temperature, PLA plastic filament will show signs of
+         * defects with a pressure of 66 PSI (4.64 kg/cm2). Read more about
+         * this, here:
+         * https://github.com/R2D2-2019/R2D2-2019/wiki/Dangerous-temperatures-for-robot-components
+         * If this is not the case, a temperature of 55 must be set instead.
+         * This is the temperature a Li-Ion battery will start to show signs of
+         * defects. Ppm means parts per million and and from 10,000 ppm it
+         * becomes dangerous. The condition is that a temperature of 400 degrees
+         * must be present.
+         * 
+         * @param base_comm_c
+         * @param int
+         * @param int
+         * */
+        hazard_detection_c(base_comm_c &base_comm,
+                           const int &tempeature_treshold = 49,
+                           const int &ppm_treshold = 10'000);
 
         void process() override;
     };
--- code/src/hazard_detection.cpp
+++ code/src/hazard_detection.cpp
@@ -1,8 +1,12 @@
 #include <hazard_detection.hpp>
 
 namespace r2d2::hazard_detection {
-    hazard_detection_c::hazard_detection_c(base_comm_c &comm)
-        : base_module_c(comm) {
+    hazard_detection_c::hazard_detection_c(base_comm_c &comm,
+                                           const int &tempeature_treshold,
+                                           const int &ppm_treshold)
+        : base_module_c(comm),
+          tempeature_treshold(tempeature_treshold),
+          ppm_treshold(ppm_treshold) {
     }
 
     void hazard_detection_c::process() {
